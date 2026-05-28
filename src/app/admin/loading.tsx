5134469ea76b666f7886871ddac46c47974a0789
Sushil <farkandesushil@gmail.com>
2026-05-28 15:42:18 +0530

    increase speed admin

---
+++ src/app/admin/loading.tsx
@@ -0,0 +1,11 @@
+export default function AdminLoading() {
+  return (
+    <div className="min-h-[60vh] flex flex-col items-center justify-center">
+      <div className="relative w-16 h-16">
+        <div className="absolute top-0 left-0 w-full h-full border-4 border-gray-200 rounded-full"></div>
+        <div className="absolute top-0 left-0 w-full h-full border-4 border-blue-600 rounded-full border-t-transparent animate-spin"></div>
+      </div>
+      <p className="mt-4 text-gray-500 font-medium animate-pulse">Processing... Please wait</p>
+    </div>
+  );
+}
